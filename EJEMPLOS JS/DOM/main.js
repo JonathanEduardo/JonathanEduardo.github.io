4b3542f3bc76b847323e953a3477848c73150bdd
JhonQuistiano <cristiano2995@gmail.com>
2017-10-11 12:12:51 -0500

    js

--- EJEMPLOS JS/DOM/main.js
+++ EJEMPLOS JS/DOM/main.js
@@ -1,6 +1,30 @@
-var caja = document.getElementsByTagName('div');
-var texto = document.createTextNode('caja4');
-nuevo.appendChild(texto);
-nuevo.setAttribute("class","caja");
-var padre = document.getElementsByTagName('body');
-padre[0].appendChild(nuevo);
+
+var btnAgregar = document.getElementById("btnAgregar");
+
+/* event listener*/
+
+btnAgregar.addEventListener('click', function(){
+    var entrada = document.getElementById('entrada');
+    var contenido = entrada.value;
+    agregaCaja(contenido);
+
+});
+/*document.addEventListener('click',agregaCaja);
+document.addEventListener('click',prueba);
+document.addEventListener('click',function (){window.alert('hola2');})*/
+
+function agregaCaja(texto){
+   // var cajas = document.getElementsByTagName('div');
+    var nuevo = document.createElement('div');
+    var texto = document.createTextNode(texto);
+    nuevo.appendChild(texto);
+    nuevo.setAttribute("class","caja");
+    var padre = document.getElementsByTagName('body');
+    padre[0].appendChild(nuevo);
+}
+
+
+function prueba()
+{
+    window.alert("hola");
+}
