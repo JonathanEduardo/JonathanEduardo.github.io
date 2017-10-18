c7e0495ebb5f108d41f6ee086e2e4db8bd892b37
JhonQuistiano <cristiano2995@gmail.com>
2017-10-18 12:13:54 -0500

    popp

--- EJEMPLOS JS/Registro/registro.js
+++ EJEMPLOS JS/Registro/registro.js
@@ -1,31 +1,62 @@
-var registro = document.getElementById('registro');
-var btnRegistrar = document.getElementById('btnRegistrar');
+var registro = document.getElementById('registro');//test.algo regresa un boolean
+var mensaje = document.getElementById('mensaje');
+var retro = document.getElementById('retro');
 
+registro.addEventListener('blur', function () {
+      verificaNombre();    
+});
+
+registro.addEventListener('keyup', function () {
+    var letras = (/a-z/i).test(registro.contra.value); //regresa un boolean
+    var digitos = (/[0-9]/i).test(registro.contra.value); 
+    var simbolos = (/[!"·$%&/()]/).test(registro.contra.value);
+
+    if(letras && digitos && simbolos){
+        retro.innerHTML ='segura';
+       // retro.className = 'nivel-mediano';
+    }
+    else if (letras && digitos) {
+        retro.innerHTML ='mediana'; 
+      //  retro.className = 'nivel-seguro';
+    } else if (letras) {
+        retro.innerHTML ='baja';
+      //  retro.className = 'nivel-bajo';
+    }
+});
 
 registro.addEventListener('submit',function(evento) {
 
-    if (!verificaNombre() || !verificaContraseña() || !verificaTerminos()) {
+    var  nom= verificaNombre();
+    var contra = verificaContraseña();
+    var terminos = verificaTerminos();
+
+    if (!nom || !contra || !terminos) {
         alert('error')
-        var mensaje = getElementById();
+       evento.preventDefault();
     }
     
-})
+});
   
 
 function verificaNombre() {
 
-    if (registro.nombre.value.charAt(0).toLower >= 'a') {
-        
+    var exReg = /[a-z]/i; //un conjunto de caracteres de la a a la z y la i es pa5ra que ignore mayusculas
+    if(exReg.test(registro.nombre.value.charAt(0)))
+    {
+        return true;
     }
-    return false;
-    
+    else{
+        mensaje.innerHTML += 'debe empezar con una letra';
+       return false;
+    }
 }
 
 function verificaContraseña() {
-    return false;
+   
+    return true;
 }
 
 
 function verificaTerminos() {
-    return false;
+    return true;
 }
